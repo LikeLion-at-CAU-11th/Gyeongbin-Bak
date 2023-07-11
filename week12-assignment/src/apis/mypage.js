98cd021feef06e471d96f75e47a42ddaa77deb8b
Gyeongbin <tarabin49@gmail.com>
2023-07-11 16:49:23 +0900

    [FEAT]Mypage.js 기능구현 mypage 내용(이름 및 나이)띄우기, 로딩화면 구현하기

---
+++ week12-assignment/src/apis/mypage.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+export const getMyPage = async() => {
+    const access = localStorage.getItem('access');
+    const result = await axios.get('http://front.cau-likelion.org/mypage', {
+        headers: {
+            Authorization: access,
+        }
+    });
+    return result.data;
+}
--- week12-assignment/src/pages/Mypage.js
+++ week12-assignment/src/pages/Mypage.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getMyPage } from '../apis/mypage';
 
 export const Mypage = () => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        getMyPage().then((res)=>{
+            setData(res);
+            setLoading(false);
+        })
+    }, []);
+    if (loading) return (<div>로딩중</div>);
   return (
-    <div>Mypage</div>
+    <div>
+        <div>{data?.name}</div>
+        <div>{data?.age}</div>
+    </div>
   )
 }
