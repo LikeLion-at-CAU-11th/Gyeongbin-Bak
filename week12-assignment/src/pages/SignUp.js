e56430652debc7504f0aca201eb101b37e24a2b3
Gyeongbin <tarabin49@gmail.com>
2023-07-11 17:50:16 +0900

    [FEAT]회원가입 페이지 구현 회원가입 페이지 중 회원가입 api를 제외하고 구현

--- week12-assignment/src/pages/SignUp.js
+++ week12-assignment/src/pages/SignUp.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { Input, Inputs, Title, Wrapper } from '../components/Common'
+import { useForm } from '../hooks/useForm'
+import { styled } from 'styled-components';
 
 export const SignUp = () => {
+    const [id, onChangeID] = useForm();
+    const [pw, onChangePW] = useForm();
+    const [name, onChangeName] = useForm();
+    const [age, onChangeAge] = useForm();
   return (
-    <div>SignUp</div>
+    <Wrapper>
+        <Title>회원가입</Title>
+        <Inputs>
+            <Input placeholder='아이디' value={id} onChange={onChangeID} />
+            <Input placeholder='비밀번호' type="password" value={pw} onChange={onChangePW} />
+            <Input placeholder='이름' value={name} onChange={onChangeName}/>
+            <Input placeholder='나이' value={age} onChange={onChangeAge}/>
+        </Inputs>
+        <Button>Sign Up</Button>
+    </Wrapper>
   )
 }
+
+const Button = styled.button`
+    background-color: black;
+    color: white;
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border-radius: 10px;
+    margin-top: 20px;
+`
